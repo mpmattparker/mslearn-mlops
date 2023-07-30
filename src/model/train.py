82ca5bff689f19cfcccbfea3d9cb4a796ece8543
Matt Parker <124211300+mpmattparker@users.noreply.github.com>
2023-07-30 09:51:48 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,6 +3,7 @@
 import argparse
 import glob
 import os
+import mlflow
 
 import pandas as pd
 
@@ -11,8 +12,8 @@ from sklearn.linear_model import LogisticRegression
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    # enable autologging
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,8 +34,15 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
 
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    data = {"train": {"X": X_train, "y": y_train},
+            "test": {"X": X_test, "y": y_test}}
+    return data
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
